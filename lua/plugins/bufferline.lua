c3361ddf79473fada9394e609f81178154afd70e
David Park <dpjungmin@gmail.com>
2023-06-13 14:13:30 -0700

    [plugin] Update plugins

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -316,9 +316,17 @@ require('bufferline').setup {
       return true
     end,
     color_icons = true,
+    get_element_icon = function(element)
+      -- element consists of {filetype: string, path: string, extension: string, directory: string}
+      -- This can be used to change how bufferline fetches the icon
+      -- for an element e.g. a buffer or a tab.
+      -- e.g.
+      local icon, hl =
+        require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+      return icon, hl
+    end,
     show_buffer_icons = false,
     show_buffer_close_icons = false,
-    show_buffer_default_icon = false,
     show_close_icon = false,
     show_tab_indicators = false,
     show_duplicate_prefix = true,
